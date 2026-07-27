04fffecc43f45b1b3dc2817df9234902811e8013
Woody-Hill <150872127+Woody-Hill@users.noreply.github.com>
2026-07-27 10:51:33 +0900

    feat: [LTC] Trapping Rain Water 풀이
    Implemented a solution for the Trapping Rain Water problem using a two-pass approach to calculate trapped water efficiently.

--- problem_solve/07-24/[LTC] Trapping Rain Water/Woody-Hill.java
+++ problem_solve/07-24/[LTC] Trapping Rain Water/Woody-Hill.java
@@ -1,10 +1,75 @@
-import java.io.*;
-import java.util.*;
+class Solution {
+    public int trap(int[] height) {
+        int n = height.length;
 
-class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(
-            new InputStreamReader(System.in)
-        );
+        int water = 0;
+
+        // Left to Right //
+        int highest = 0;
+        int secondHighest = 0;
+
+        int firstIndex = -1;
+        int secondIndex = -1;
+
+        for (int i = 0; i < n; i++) {
+            int h = height[i];
+
+            if (h >= highest) {
+                
+                secondHighest = highest;
+                secondIndex = firstIndex;
+
+                highest = h;
+                firstIndex = i;
+
+                for (int idx = firstIndex - 1; idx > secondIndex; idx--) {
+                    water += secondHighest - height[idx];
+                }
+
+                secondHighest = 0;
+                secondIndex = -1;
+                
+            } else if (h >= secondHighest) {
+                secondHighest = h;
+                secondIndex = i;
+            }
+        }
+        // ------------- // 
+
+        // Right to Left //
+        int boundary = firstIndex;
+
+        highest = 0;
+        secondHighest = 0;
+
+        firstIndex = -1;
+        secondIndex = -1;
+
+        for (int i = n - 1; i >= boundary; i--) {
+            int h = height[i];
+
+            if (h >= highest) {
+                
+                secondHighest = highest;
+                secondIndex = firstIndex;
+
+                highest = h;
+                firstIndex = i;
+
+                for (int idx = firstIndex + 1; idx < secondIndex; idx++) {
+                    water += secondHighest - height[idx];
+                }
+
+                secondHighest = 0;
+                secondIndex = -1;
+                
+            } else if (h >= secondHighest) {
+                secondHighest = h;
+                secondIndex = i;
+            }
+        }
+        // -------------- //
+
+        return water;
     }
 }
