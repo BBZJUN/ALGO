1f91bd61d6239438a2c4a61c926857705922b291
Seyeon Kim <102506114+yonseeee@users.noreply.github.com>
2026-07-29 12:57:51 +0900

    Solve: [PGS] - 두 큐 합 같게 만들기

--- problem_solve/07-29/[PGS] 두 큐 합 같게 만들기/yonseeee.java
+++ problem_solve/07-29/[PGS] 두 큐 합 같게 만들기/yonseeee.java
@@ -4,6 +4,40 @@ class Solution {
     public int solution(int[] queue1, int[] queue2) {
         int answer = 0;
         
+        Queue<Integer> q1 = new ArrayDeque<>();
+        Queue<Integer> q2 = new ArrayDeque<>();
+        
+        long sum1=0, sum2=0;
+        for(int value:queue1){
+            sum1+=value;
+            q1.offer(value);
+        }
+        for(int value:queue2){
+            sum2+=value;
+            q2.offer(value);
+        }
+        
+        long half = (sum1+sum2)/2;
+        
+
+        while(sum1!=sum2){
+            if(sum1<half){
+                int elem=q2.poll();
+                q1.offer(elem);
+                
+                sum1+=elem; sum2-=elem;
+            }else if(sum2<half){
+                int elem=q1.poll();
+                q2.offer(elem);
+                
+                sum1-=elem; sum2+=elem;
+            }
+            answer++;
+            if(answer>queue1.length*4){
+                return -1;
+            }
+        }
+        
         return answer;
     }
 }
