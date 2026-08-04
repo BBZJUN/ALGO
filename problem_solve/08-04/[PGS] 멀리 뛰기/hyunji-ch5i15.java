07775461f804c17498626e9c9d054d0aa42011d4
최현지 <ch5i_hj15@naver.com>
2026-08-04 11:02:39 +0900

    데일리 - 1문제

--- problem_solve/08-04/[PGS] 멀리 뛰기/hyunji-ch5i15.java
+++ problem_solve/08-04/[PGS] 멀리 뛰기/hyunji-ch5i15.java
@@ -1,10 +1,19 @@
-import java.io.*;
 import java.util.*;
 
-class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(
-            new InputStreamReader(System.in)
-        );
+class Solution {
+    public long solution(int n) {
+        long answer = 0;
+        // 엣지케이스 처리
+        if (n == 1)
+            return 1;
+        // dp 정의: n을 만들 수 있는 경우의 수
+        long[] dp = new long[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
+        }
+
+        return dp[n];
     }
 }
