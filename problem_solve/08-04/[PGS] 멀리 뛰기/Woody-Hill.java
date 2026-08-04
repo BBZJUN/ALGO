0ef3de51e9b8c7298e0298b53fcd95a5f0d3e85e
Woody-Hill <150872127+Woody-Hill@users.noreply.github.com>
2026-08-05 06:45:30 +0900

    feat: [PGS] 멀리 뛰기 풀이
    Refactored the main class to a Solution class and implemented a Fibonacci solution method.

--- problem_solve/08-04/[PGS] 멀리 뛰기/Woody-Hill.java
+++ problem_solve/08-04/[PGS] 멀리 뛰기/Woody-Hill.java
@@ -1,10 +1,14 @@
-import java.io.*;
-import java.util.*;
-
-class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(
-            new InputStreamReader(System.in)
-        );
+class Solution {
+    public long solution(int n) {
+        
+        long[] fibonacci = new long[n + 1];
+        fibonacci[0] = 1;
+        fibonacci[1] = 1;
+        
+        for (int i = 2; i <= n; i++) {
+            fibonacci[i] = (fibonacci[i - 1] + fibonacci[i - 2]) % 1234567;
+        }
+        
+        return fibonacci[n];
     }
 }
