3747776834733f6fab7fa31779f3be2a715fa584
qkrcjsdlr10 <qkrcjsdlr10@gmail.com>
2026-08-12 22:06:29 +0900

    데일리 - 1문제

--- problem_solve/08-12/[PGS] 정수 삼각형/qkrcjsdlr10.java
+++ problem_solve/08-12/[PGS] 정수 삼각형/qkrcjsdlr10.java
@@ -13,34 +13,18 @@ class Solution {
         
         for(int i = 1; i < triangle.length; i++){
             for(int j = 0; j < triangle[i].length; j++){
-<<<<<<< HEAD
-=======
-                // System.out.print(dp[i][j] + " ");
->>>>>>> 9a9f4f25731aece64b1e8cf9e8f4c2e5cc0a798e
                 if(j == 0){
                     dp[i][j] = dp[i-1][j] + dp[i][j];
                     continue;
                 }
                 dp[i][j] = Math.max(dp[i - 1][j] + dp[i][j], dp[i - 1][j - 1] + dp[i][j]);
             }
-<<<<<<< HEAD
-=======
-            System.out.println();
->>>>>>> 9a9f4f25731aece64b1e8cf9e8f4c2e5cc0a798e
         }
         
         
         for(int j = 0; j < n; j++){
-<<<<<<< HEAD
             answer = Math.max(answer, dp[n-1][j]);
         }
-=======
-            // System.out.print(dp[i][j] + " ");
-            answer = Math.max(answer, dp[n-1][j]);
-        }
-        // System.out.println();
-        
->>>>>>> 9a9f4f25731aece64b1e8cf9e8f4c2e5cc0a798e
         
         return answer;
     }
