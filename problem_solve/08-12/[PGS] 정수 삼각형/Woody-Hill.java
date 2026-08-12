93023f07d04d3a74a89620e13505f4a2f0dd9d7b
Woody-Hill <150872127+Woody-Hill@users.noreply.github.com>
2026-08-12 15:52:25 +0900

    fix: 정수 삼각형 풀이 수정
    Initialize maxNum with the value from the top of the pyramid.

--- problem_solve/08-12/[PGS] 정수 삼각형/Woody-Hill.java
+++ problem_solve/08-12/[PGS] 정수 삼각형/Woody-Hill.java
@@ -3,10 +3,10 @@ class Solution {
         int n = triangle.length;
         int[][] pyramid = new int[n][n]; // DP 배열
         
-        int maxNum = 0;
-        
         // 최상층만 처리해 두고 아래로 내려가면서 더하기
         pyramid[0][0] = triangle[0][0];
+        
+        int maxNum = pyramid[0][0];      // n = 1 대비
         
         for (int lvl = 1; lvl < n; lvl++) {
             for (int idx = 0; idx <= lvl; idx++) {
