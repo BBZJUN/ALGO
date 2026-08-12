96b2cbf7f3d803e99b6a7740c50bc23566fe0579
이영은 <121778825+ye0ngeun@users.noreply.github.com>
2026-08-12 18:59:26 +0900

    [PGS] 정수 삼각형 / ye0ngeun / Lv3

--- problem_solve/08-12/[PGS] 정수 삼각형/ye0ngeun.java
+++ problem_solve/08-12/[PGS] 정수 삼각형/ye0ngeun.java
@@ -1,10 +1,29 @@
-import java.io.*;
-import java.util.*;
+class Solution {
+    public int solution(int[][] triangle) {
+        for (int i = 1; i < triangle.length; i++) {
+            for (int j = 0; j < triangle[i].length; j++) {
+                // 맨 왼쪽
+                if (j == 0) {
+                    triangle[i][j] += triangle[i-1][j];
+                } 
+                // 맨 오른쪽
+                else if (j == i) {
+                    triangle[i][j] += triangle[i-1][j-1];
+                }
+                // 가운데
+                else {
+                    triangle[i][j] += Math.max(triangle[i-1][j-1], triangle[i-1][j]);
+                }
+            }
+        }
+        
+        int[] lastRow = triangle[triangle.length - 1];
+        int answer = 0;
 
-class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(
-            new InputStreamReader(System.in)
-        );
+        for (int value : lastRow) {
+            answer = Math.max(answer, value);
+        }
+        
+        return answer;
     }
 }
