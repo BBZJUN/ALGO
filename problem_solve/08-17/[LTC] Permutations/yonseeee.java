353184e4ecba510bac719bd68f54c8aa310a27c4
Seyeon Kim <102506114+yonseeee@users.noreply.github.com>
2026-08-18 07:57:21 +0900

    Solve: [LTC] - Permutations
    Implemented a depth-first search (DFS) algorithm to generate all permutations of an integer array.

--- problem_solve/08-17/[LTC] Permutations/yonseeee.java
+++ problem_solve/08-17/[LTC] Permutations/yonseeee.java
@@ -1,8 +1,31 @@
-import java.io.*;
-import java.util.*;
+class Solution {
+    List<List<Integer>> result;
+    public List<List<Integer>> permute(int[] nums) {
+        
+        result=new ArrayList<>();
+        dfs(new ArrayList<>(), nums, new boolean[nums.length]);
 
-class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        return result;
+    }
+
+    public void dfs(List<Integer> current, int[]nums, boolean[] visited){
+
+        if(current.size()==nums.length){
+            result.add(new ArrayList<>(current));
+            return;
+        }
+
+        for(int i=0;i<nums.length;i++){
+            if(!visited[i]){
+                current.add(nums[i]);
+                visited[i]=true;
+
+                dfs(current, nums, visited);
+
+                current.remove(current.size()-1);
+                visited[i]=false;
+            }
+        }
+
     }
 }
